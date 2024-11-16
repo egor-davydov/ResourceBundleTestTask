224869c00a7726e198f1d32e6c21c80f5f74b7f6
Egor Davydov <a.egordavidov@gmail.com>
2024-11-16 14:59:42 +0300

    make ResourceBundleWindow UI

---
+++ Assets/Code/Editor/TabsHelperEditor.cs
@@ -0,0 +1,45 @@
+﻿using Code.Helpers;
+using UnityEditor;
+using UnityEngine;
+
+namespace Code.Editor
+{
+  [CustomEditor(typeof(TabsHelper))]
+  public class TabsHelperEditor : UnityEditor.Editor
+  {
+    private const int MinimumTabs = 2;
+    
+    private TabsHelper _tabsHelper;
+
+    private void OnEnable() => 
+      _tabsHelper = (TabsHelper)target;
+
+    public override void OnInspectorGUI()
+    {
+      serializedObject.Update();
+      DrawPropertiesExcluding(serializedObject, "m_Script");
+      if (_tabsHelper.Tabs.Count > MinimumTabs - 1)
+      {
+        foreach (GameObject tab in _tabsHelper.Tabs)
+        {
+          GUI.enabled = !tab.activeSelf;
+          if (GUILayout.Button($"Activate {tab.name}"))
+          {
+            ActivateOnlyOneTab(tabToActivate: tab);
+            EditorUtility.SetDirty(_tabsHelper);
+          }
+        }
+      }
+      else
+        EditorGUILayout.HelpBox($"Need at least \"{MinimumTabs}\" tabs", MessageType.Warning);
+
+      serializedObject.ApplyModifiedProperties();
+    }
+
+    private void ActivateOnlyOneTab(GameObject tabToActivate)
+    {
+      foreach (GameObject tab in _tabsHelper.Tabs)
+        tab.SetActive(tab == tabToActivate);
+    }
+  }
+}
---
+++ Assets/Code/Helpers/TabsHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.Helpers
+{
+  public class TabsHelper : MonoBehaviour
+  {
+    [field: SerializeField] public List<GameObject> Tabs { get; private set; } = new List<GameObject>(3);
+
+    private void OnValidate() => 
+      CheckForDuplicates();
+
+    private void CheckForDuplicates()
+    {
+      for (int i = 0; i < Tabs.Count; i++)
+      for (int j = i + 1; j < Tabs.Count; j++)
+      {
+        GameObject firstTab = Tabs[i];
+        GameObject secondTab = Tabs[j];
+        if (firstTab == secondTab)
+          Tabs.Remove(firstTab);
+      }
+    }
+
+  }
+}
